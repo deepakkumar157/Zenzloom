b36b438efefe143728e0682b6bd5012e1b7ad7d9
Deepak Gupta <dk6861614@gmail.com>
2026-04-26 23:24:42 +0530

    Add Dockerfile, docker-compose.yml and updated README

--- MERN-E-Commerce-Store-main/MERN-E-Commerce-Store-main/backend/index.js
+++ MERN-E-Commerce-Store-main/MERN-E-Commerce-Store-main/backend/index.js
@@ -41,4 +41,12 @@ app.get("/api/config/paypal", (req, res) => {
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 app.listen(port, () => console.log(`Server running on port: ${port}`));
